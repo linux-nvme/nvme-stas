2cfd71b6cf59840e74ece59476f51ec2499a383b
Boris Glimcher <Boris.Glimcher@emc.com>
2022-04-13 19:09:20 +0300

    doc: post documentation to RTD
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

---
+++ doc/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'nvme-stas'
+copyright = '2020, Martin Belanger'
+author = 'Martin Belanger <martin.belanger@dell.com>'
+master_doc = 'index'
+
+release = '1.0'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['html', 'man', 'Thumbs.db', '.DS_Store']
---
+++ doc/index.rst
@@ -0,0 +1,20 @@
+.. nvme-stas documentation master file, created by
+   sphinx-quickstart on Wed Apr 13 19:07:37 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to nvme-stas's documentation!
+=====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
