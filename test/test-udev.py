9382a4ef687f6b63301b1ec70a5ca0b259d2a5fb
Boris Glimcher <Boris.Glimcher@emc.com>
2022-05-26 13:56:12 +0300

    test: change if name guardian to allow manual test execution
    example:
    
    $ LD_LIBRARY_PATH=.build/subprojects/libnvme/src python3 -m unittest test/*.py
    .................Udev.get_nvme_device() - Error: [Errno 2] No such file or directory: '/dev/bozo'
    .........
    ----------------------------------------------------------------------
    Ran 26 tests in 0.004s
    
    OK
    
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- test/test-udev.py
+++ test/test-udev.py
@@ -3,19 +3,20 @@ import os
 import unittest
 from staslib import stas
 
+
+class Test(unittest.TestCase):
+    '''Unit tests for class Udev'''
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def test_get_device(self):
+        dev = stas.UDEV.get_nvme_device('null')
+        self.assertEqual(dev.device_node, '/dev/null')
+
+    def test_get_bad_device(self):
+        self.assertIsNone(stas.UDEV.get_nvme_device('bozo'))
+
+
 if __name__ == '__main__':
-
-    class Test(unittest.TestCase):
-        '''Unit tests for class Udev'''
-
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-
-        def test_get_device(self):
-            dev = stas.UDEV.get_nvme_device('null')
-            self.assertEqual(dev.device_node, '/dev/null')
-
-        def test_get_bad_device(self):
-            self.assertIsNone(stas.UDEV.get_nvme_device('bozo'))
-
     unittest.main()
